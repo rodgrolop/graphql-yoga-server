3adb7c18d1a0041e5b21d73b3c89ffa1b0e700f1
Rodrigo Gross <rodrigogrosslopez@gmail.com>
2019-03-13 10:33:55 +0100

    schemas modularizing with babel imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { prisma } from './generated/prisma-client'
 import resolvers from './resolvers'
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema/schema.graphql',
+  typeDefs: './src/schema/combinedSchema.graphql',
   resolvers,
   context: request => {
     return {
